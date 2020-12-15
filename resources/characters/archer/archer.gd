994bf434be302683bf2990966f988afe273d7662
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2020-12-15 15:15:06 +0100

    refactored damage number position code and commented

--- resources/characters/archer/archer.gd
+++ resources/characters/archer/archer.gd
@@ -4,11 +4,13 @@ export (PackedScene) var Arrow
 
 
 func _ready():
+#	Connects BattleUI's attack signal to a function in this script.
 	var BattleUI = get_tree().get_root().find_node("BattleUI", true, false)
 	BattleUI.connect("attack", self, "handleattack")
 
 
 func handleattack():
+#	Runs when BattleUI emits the "attack" signal.
 	var arrow = Arrow.instance()
 	add_child(arrow)
 	arrow.position.y -= 4
