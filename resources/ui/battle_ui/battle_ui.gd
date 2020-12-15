04d7b34226d266e86cfd4a36c2fc1383265cfd15
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2020-12-15 10:55:36 +0100

    archer now shoots an arrow on attack

--- resources/ui/battle_ui/battle_ui.gd
+++ resources/ui/battle_ui/battle_ui.gd
@@ -1,20 +1,12 @@
 extends MarginContainer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal attack
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _on_AttackButton_button_down():
 	self.hide()
-	pass # Replace with function body.
+	emit_signal("attack")
