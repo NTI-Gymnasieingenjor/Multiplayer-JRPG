9cdf043028dd62959f2e1a51e1640c1baf2f007f
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2020-12-17 10:45:46 +0100

    edited output text in test_damage_numbers.gd

--- test/unit/test_damage_numbers.gd
+++ test/unit/test_damage_numbers.gd
@@ -8,9 +8,10 @@ func before_each():
 	damagenumber = load("res://resources/damagenumbers/damage_numbers.tscn").instance()
 	king = load("res://resources/characters/king/king.tscn").instance()
 
+
 func test_show_damage_numbers():
 	king._on_King_area_shape_entered(null, null, null, null)
 	if damagenumber.is_visible():
-		_pass("Damage was displayed after the king got hit by the arrow.")
+		_pass("Damage numbers display after King collides with object")
 	else:
-		_fail("Damage was not displayed after the king got hit by the arrow.")
+		_fail("Damage numbers does not display after King collides with object")
