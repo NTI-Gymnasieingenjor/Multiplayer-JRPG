f7c72d8590dd3844a67175b635bf321afd653b1a
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-01-19 15:10:42 +0100

    Fluff (#18)
    * fixed bug where BattleUI showed up too early
    
    * changed background to a better looking one
    
    * replaced the old king enemy with an animated knight

--- resources/damagenumbers/damage_numbers.gd
+++ resources/damagenumbers/damage_numbers.gd
@@ -13,7 +13,4 @@ func show_value(value, duration):
 	$Tween.start()
 	yield($Tween, "tween_all_completed")
 	
-#	Temporary battle loop solution
-	get_parent().get_parent().get_parent().get_node("BattleUI").show()
-	
 	queue_free()
