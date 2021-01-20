1513841b2bd4dde9e18be086e757030a9637af1e
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-01-20 09:22:52 +0100

    waits for attack button on player turn

--- resources/turn_queue.gd
+++ resources/turn_queue.gd
@@ -6,20 +6,21 @@ var active_character
 
 func _ready():
 	active_character = get_child(0)
-	play_turn()
+	battle_loop()
 
-func play_turn():
+func battle_loop():
+	var battleui = get_tree().get_root().find_node("BattleUI", true, false)
+	
 	while true:
-	#	match active_character.get("is_enemy"):
-	#		true:
-	#			pass
-	#		false:
-	#			pass
+		match active_character.get("is_enemy"):
+			true:
+				pass
+			false:
+				battleui.show()
+				yield(battleui, "attack")
+		
 		active_character.play_turn()
 		yield(active_character.get_node("AnimatedSprite"), "animation_finished")
 
 		var new_index : int = (active_character.get_index() + 1) % get_child_count()
 		active_character = get_child(new_index)
-
-#		if active_character.get("is_enemy") == false:
-#			get_tree().get_root().find_node("BattleUI", true, false).show()
