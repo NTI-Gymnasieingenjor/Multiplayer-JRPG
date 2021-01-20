c6db933bdd8ab75af0793f0310e442f6d4599e79
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-01-20 13:13:02 +0100

    plays opening audio once, then loops main part

--- resources/audio/dynamic_audio.gd
+++ resources/audio/dynamic_audio.gd
@@ -3,5 +3,10 @@ extends Node2D
 func _ready():
 	var player = AudioStreamPlayer.new()
 	self.add_child(player)
+	player.stream = load("res://resources/audio/boss_battle_#2_metal_opening.ogg")
+	player.play()
+	
+	yield(player, "finished")
+	
 	player.stream = load("res://resources/audio/boss_battle_#2_metal_loop.ogg")
 	player.play()
