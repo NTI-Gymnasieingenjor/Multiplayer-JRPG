5c2a61607c418d6b57ea8d738ec3e9ff7eb15218
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-01-20 09:36:07 +0100

    changed knight idle animation from 5fps to 10fps

--- resources/enemies/knight/knight.gd
+++ resources/enemies/knight/knight.gd
@@ -21,19 +21,16 @@ func _ready():
 var rng = RandomNumberGenerator.new()
 func handleattack():
 	var active_character = get_tree().get_root().find_node("TurnQueue", true, false).get("active_character")
-	
 	var attackname = active_character.get("attack")
 	
 	db.open_db()
 	db.query("SELECT * FROM attacks WHERE name='" + attackname + "';")
-	
 	var mindamage = db.query_result[0]["mindamage"]
 	var maxdamage = db.query_result[0]["maxdamage"]
 	
 	yield(active_character.get_node("AnimatedSprite"), "animation_finished")
 
 	rng.randomize()
-	
 	$DamageManager.show_value(rng.randi_range(mindamage, maxdamage))
 
 
