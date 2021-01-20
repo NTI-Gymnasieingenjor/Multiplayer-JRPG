a5b05a4e3a786ac3a4595ce34d37db967aaef8e1
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-01-20 16:03:32 +0100

    added attack sound effect for enemy

--- resources/dynamic_audio.gd
+++ resources/dynamic_audio.gd
@@ -3,11 +3,11 @@ extends Node2D
 func _ready():
 	var player = AudioStreamPlayer.new()
 	self.add_child(player)
-	player.stream = load("res://resources/audio/boss_battle_#2_metal_opening.ogg")
+	player.stream = load("res://resources/bgm/boss_battle/boss_battle_#2_metal_opening.ogg")
 	player.set_volume_db(-5.0)
 	player.play()
 	
 	yield(player, "finished")
 	
-	player.stream = load("res://resources/audio/boss_battle_#2_metal_loop.ogg")
+	player.stream = load("res://resources/bgm/boss_battle/boss_battle_#2_metal_loop.ogg")
 	player.play()
--- resources/enemies/knight/knight.gd
+++ resources/enemies/knight/knight.gd
@@ -40,6 +40,14 @@ func take_damage():
 
 
 func play_turn():
+	var dynamic_audio = get_tree().get_root().find_node("DynamicAudio", true, false)
+	var player = AudioStreamPlayer.new()
+	
+	dynamic_audio.add_child(player)
+	player.stream = load("res://resources/enemies/knight/" + attack + ".ogg")
+	
 	$AnimatedSprite.play("Attack")
+	player.play()
 	yield(get_node("AnimatedSprite"), "animation_finished")
+	player.stop()
 	$AnimatedSprite.play("Idle")
