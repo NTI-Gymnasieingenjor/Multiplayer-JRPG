0e348cdcd05f1e0a7dfd0414048e2952498f3c21
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-01-21 14:37:33 +0100

    victory screen, beautified code, added comments

--- resources/characters/archer/archer.gd
+++ resources/characters/archer/archer.gd
@@ -2,6 +2,7 @@ extends Position2D
 
 var is_enemy = false
 var attack = "archermelee"
+var hp
 
 
 func _ready():
@@ -9,14 +10,22 @@ func _ready():
 
 
 func play_turn():
+#	Creates new AudioStreamPlayer instance.
 	var dynamic_audio = get_tree().get_root().find_node("DynamicAudio", true, false)
 	var player = AudioStreamPlayer.new()
-	
 	dynamic_audio.add_child(player)
+	
+#	Loads attack sound effect.
 	player.stream = load("res://resources/characters/archer/" + attack + ".ogg")
 	
+#	Plays attack animation and sound.
 	$AnimatedSprite.play("Attack")
 	player.play()
+	
 	yield(get_node("AnimatedSprite"), "animation_finished")
+	
+#	Stops sound effect and returns to idle animation.
 	player.stop()
 	$AnimatedSprite.play("Idle")
+	
+	player.queue_free()
