256566bafa27677965814e6e9d487b26957029dd
Albin Nyman <joshei@telia.com>
2021-01-25 10:59:16 +0100

    Added class inheritance for the player characters

--- resources/characters/archer/archer.gd
+++ resources/characters/archer/archer.gd
@@ -1,30 +1,5 @@
-extends Position2D
-
-var is_enemy = false
-var attack = "archermelee"
+extends "res://resources/characters/base/player.gd"
 
 
 func _ready():
-	$AnimatedSprite.play("Idle")
-
-
-func play_turn():
-#	Creates new AudioStreamPlayer instance.
-	var dynamic_audio = get_tree().get_root().find_node("DynamicAudio", true, false)
-	var player = AudioStreamPlayer.new()
-	dynamic_audio.add_child(player)
-	
-#	Loads attack sound effect.
-	player.stream = load("res://resources/characters/archer/" + attack + ".ogg")
-	
-#	Plays attack animation and sound.
-	$AnimatedSprite.play("Attack")
-	player.play()
-	
-	yield(get_node("AnimatedSprite"), "animation_finished")
-	
-#	Stops sound effect and returns to idle animation.
-	player.stop()
-	$AnimatedSprite.play("Idle")
-	
-	player.queue_free()
+	attack = "archermelee"
---
+++ resources/characters/base/player.gd
@@ -0,0 +1,29 @@
+extends Position2D
+
+var is_enemy = false
+var attack
+
+func _ready():
+	$AnimatedSprite.play("Idle")
+
+
+func play_turn():
+#	Creates new AudioStreamPlayer instance.
+	var dynamic_audio = get_tree().get_root().find_node("DynamicAudio", true, false)
+	var player = AudioStreamPlayer.new()
+	dynamic_audio.add_child(player)
+	
+#	Loads attack sound effect.
+	player.stream = load(self.get_filename().replace(self.get_name().to_lower() + ".tscn", attack + ".ogg"))
+	
+#	Plays attack animation and sound.
+	$AnimatedSprite.play("Attack")
+	player.play()
+	
+	yield(get_node("AnimatedSprite"), "animation_finished")
+	
+#	Stops sound effect and returns to idle animation.
+	player.stop()
+	$AnimatedSprite.play("Idle")
+	
+	player.queue_free()
