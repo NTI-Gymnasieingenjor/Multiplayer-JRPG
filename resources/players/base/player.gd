890ccf721960c36b9e76fdd49d640bb04229f316
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-01-27 14:53:36 +0100

    Simple attack timing system

--- resources/players/base/player.gd
+++ resources/players/base/player.gd
@@ -1,6 +1,6 @@
 extends "res://resources/character.gd"
 
-signal successful_timing
+signal button_pressed
 
 const TimedButton = preload("res://resources/ui/timed_button/timed_button.tscn")
 var button
@@ -20,9 +20,21 @@ func timed_button():
 #	Creates new TimedButton instance and sets its position accordingly.
 	button = TimedButton.instance()
 	button.rect_position.x += 25
+	var buttonsprite = button.get_node("AnimatedSprite")
 	self.add_child(button)
 	
+#	Plays button fade in animation.
+	buttonsprite.play("Approach")
 	yield(button, "button_down")
 	
+	if buttonsprite.frame == 1:
+		buttonsprite.play("Hit")
+		timing = "hit"
+	else:
+		buttonsprite.play("Miss")
+		timing = "miss"
+		
+	yield(buttonsprite, "animation_finished")
+	
 	button.queue_free()
-	emit_signal("successful_timing")
+	emit_signal("button_pressed")
